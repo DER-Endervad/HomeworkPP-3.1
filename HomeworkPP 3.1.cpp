f8278e6a076fb778e54d19f23b88b25dfb3b7285
Даниил <derendervad@gmail.com>
2023-02-23 19:39:47 +0300

    initial commit

---
+++ HomeworkPP 3.1.cpp
@@ -0,0 +1,56 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+class smart_array {
+private:
+	int* arr;
+	int size = 0;
+	int queue = 0;
+public:
+	smart_array(int input) {
+		this->size = input;
+		arr = new int[size];
+	}
+
+	void add_element(int input) {
+		if (queue < size) {
+			arr[queue] = input;
+			++queue;
+		}
+		else {
+			 throw std::runtime_error("Превышен размер массива");
+		}
+	}
+
+	int get_element(int input) {
+		if (input >= 0 && input < size) {
+			return arr[input];
+		}
+		else {
+			 throw std::runtime_error("Ошибка, указан неправильный индекс элемента.");
+		}
+	}
+
+	~smart_array() {
+		delete arr;
+	}
+};
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	try {
+		smart_array arr(5);
+		arr.add_element(1);
+		arr.add_element(4);
+		arr.add_element(155);
+		arr.add_element(14);
+		arr.add_element(15);
+		std::cout << arr.get_element(2) << std::endl;
+	}
+	catch (const std::exception& ex) {
+		std::cout << ex.what() << std::endl;
+	}
+}
